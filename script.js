1df08089daf411fb1d0080fbb66825bb8ccfd3cf
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-04-05 15:38:19 +0300

    close menu on tap

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const humbergerBtn = document.querySelector('.fa-bars');
 const timesBtn = document.querySelector('.fa-times');
 const menuBar = document.querySelector('.menu-bar');
+const anyMenu = document.querySelectorAll('.menu-bar')
 
 humbergerBtn.addEventListener('click', () => {
   menuBar.style.display = 'block';
@@ -8,3 +9,7 @@ humbergerBtn.addEventListener('click', () => {
 timesBtn.addEventListener('click', () => {
   menuBar.style.display = 'none';
 });
+
+anyMenu.forEach(n => n.addEventListener("click", () => {
+  menuBar.style.display ="none";
+}))
