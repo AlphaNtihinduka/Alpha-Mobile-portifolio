e7fb07028f90f26dfcd36e9051696fe072bc1048
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-04-07 18:42:42 +0300

    correct js linters

--- popup.js
+++ popup.js
@@ -1,82 +1,81 @@
-const btnPopup1 = document.querySelector(".card-1-btn");
-const btnPopup2 = document.querySelector(".card-2-btn");
-const btnPopup3 = document.querySelector(".card-3-btn");
-const btnPopup4 = document.querySelector(".card-4-btn");
-const crossBtn = document.querySelector(".fa-times");
-const popupContainer = document.querySelector(".popup-container");
-const overLayContainer = document.querySelector(".overlay-container");
-const newElement = document.createElement("div");
+const btnPopup1 = document.querySelector('.card-1-btn');
+const btnPopup2 = document.querySelector('.card-2-btn');
+const btnPopup3 = document.querySelector('.card-3-btn');
+const btnPopup4 = document.querySelector('.card-4-btn');
+const popupContainer = document.querySelector('.popup-container');
+const overLayContainer = document.querySelector('.overlay-container');
+const newElement = document.createElement('div');
 popupContainer.append(newElement);
-newElement.setAttribute("class", "pop-container-child");
-const anypop = document.querySelectorAll(".overlay-container")
-let popupObj = [
+newElement.setAttribute('class', 'pop-container-child');
+const anypop = document.querySelectorAll('.overlay-container');
+const popupObj = [
   {
-    name: "Tonic",
-    canopy_title: "CANOPY",
-    back_end: "Back End Dev",
-    year : "2015",
+    name: 'Tonic',
+    canopy_title: 'CANOPY',
+    back_end: 'Back End Dev',
+    year: '2015',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    featured_image: "./images/SnapshootPortfolio.png",
-    technologies: ["HTML", "CSS", "JavaScript"],
-    link_live: "https://alphantihinduka.github.io/Alpha-Mobile-portifolio/",
-    link_source: "https://github.com/AlphaNtihinduka/Alpha-Mobile-portifolio",
-    live_icon: "./images/icon.png",
-    source_icon: "./images/vector.png"
+    featured_image: './images/SnapshootPortfolio.png',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    link_live: 'https://alphantihinduka.github.io/Alpha-Mobile-portifolio/',
+    link_source: 'https://github.com/AlphaNtihinduka/Alpha-Mobile-portifolio',
+    live_icon: './images/icon.png',
+    source_icon: './images/vector.png',
   },
 
   {
-    name: "Multi-Post Stories",
-    canopy_title: "CANOPY",
-    back_end: "Back End Dev",
-    year : "2015",
+    name: 'Multi-Post Stories',
+    canopy_title: 'CANOPY',
+    back_end: 'Back End Dev',
+    year: '2015',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    featured_image: "./images/SnapshootCard2.png",
-    technologies: ["HTML", "Ruby on Rail", "CSS", "JavaScript"],
-    link_live: "https://alphantihinduka.github.io/Alpha-Mobile-portifolio/",
-    link_source: "https://github.com/AlphaNtihinduka/Alpha-Mobile-portifolio",
-    live_icon: "./images/icon.png",
-    source_icon: "./images/vector.png"
+    featured_image: './images/SnapshootCard2.png',
+    technologies: ['HTML', 'Ruby on Rail', 'CSS', 'JavaScript'],
+    link_live: 'https://alphantihinduka.github.io/Alpha-Mobile-portifolio/',
+    link_source: 'https://github.com/AlphaNtihinduka/Alpha-Mobile-portifolio',
+    live_icon: './images/icon.png',
+    source_icon: './images/vector.png',
   },
 
   {
-    name: "Facebook 360",
-    canopy_title: "CANOPY",
-    back_end: "Back End Dev",
-    year : "2015",
+    name: 'Facebook 360',
+    canopy_title: 'CANOPY',
+    back_end: 'Back End Dev',
+    year: '2015',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    featured_image: "./images/SnapshootCard3.png",
-    technologies: ["HTML", "Ruby on Rail", "CSS", "JavaScript"],
-    link_live: "https://alphantihinduka.github.io/Alpha-Mobile-portifolio/",
-    link_source: "https://github.com/AlphaNtihinduka/Alpha-Mobile-portifolio",
-    live_icon: "./images/icon.png",
-    source_icon: "./images/vector.png"
+    featured_image: './images/SnapshootCard3.png',
+    technologies: ['HTML', 'Ruby on Rail', 'CSS', 'JavaScript'],
+    link_live: 'https://alphantihinduka.github.io/Alpha-Mobile-portifolio/',
+    link_source: 'https://github.com/AlphaNtihinduka/Alpha-Mobile-portifolio',
+    live_icon: './images/icon.png',
+    source_icon: './images/vector.png',
   },
 
   {
-    name: "Uber Navigation",
-    canopy_title: "CANOPY",
-    back_end: "Back End Dev",
-    year : "2015",
+    name: 'Uber Navigation',
+    canopy_title: 'CANOPY',
+    back_end: 'Back End Dev',
+    year: '2015',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    featured_image: "./images/SnapshootCard4.png",
-    technologies: ["HTML", "Ruby on Rail", "CSS", "JavaScript"],
-    link_live: "https://alphantihinduka.github.io/Alpha-Mobile-portifolio/",
-    link_source: "https://github.com/AlphaNtihinduka/Alpha-Mobile-portifolio",
-    live_icon: "./images/icon.png",
-    source_icon: "./images/vector.png"
+    featured_image: './images/SnapshootCard4.png',
+    technologies: ['HTML', 'Ruby on Rail', 'CSS', 'JavaScript'],
+    link_live: 'https://alphantihinduka.github.io/Alpha-Mobile-portifolio/',
+    link_source: 'https://github.com/AlphaNtihinduka/Alpha-Mobile-portifolio',
+    live_icon: './images/icon.png',
+    source_icon: './images/vector.png',
   },
 ];
 
 let objtracker;
 
-btnPopup1.addEventListener("click", () => {
+btnPopup1.addEventListener('click', () => {
   objtracker = 0;
 
-  let output = "";
+  let output = '';
 
   popupObj[objtracker].technologies.forEach((techContent) => {
     output += `
@@ -109,25 +108,25 @@ btnPopup1.addEventListener("click", () => {
             </div>
         </div>
    `;
-   overLayContainer.style.display = "block"
-  popupContainer.style.display = "block";
+  overLayContainer.style.display = 'block';
+  popupContainer.style.display = 'block';
 });
-anypop.forEach(n => n.addEventListener("click", () => {
-    overLayContainer.style.display ="none";
-  }))
+anypop.forEach((n) => n.addEventListener('click', () => {
+  overLayContainer.style.display = 'none';
+}));
 
-  btnPopup2.addEventListener("click", () => {
-    objtracker = 1;
-  
-    let output = "";
-  
-    popupObj[objtracker].technologies.forEach((techContent) => {
-      output += `
+btnPopup2.addEventListener('click', () => {
+  objtracker = 1;
+
+  let output = '';
+
+  popupObj[objtracker].technologies.forEach((techContent) => {
+    output += `
           <span> ${techContent} </span>
       `;
-    });
-  
-    newElement.innerHTML = `
+  });
+
+  newElement.innerHTML = `
           <i class="fa fa-times" id="times"></i>
           <h2 class="popup_title">${popupObj[objtracker].name}</h2>
           <div class="prjct-info">
@@ -152,22 +151,22 @@ anypop.forEach(n => n.addEventListener("click", () => {
               </div>
           </div>
      `;
-     overLayContainer.style.display = "block";
-    popupContainer.style.display = "block";
-  });
+  overLayContainer.style.display = 'block';
+  popupContainer.style.display = 'block';
+});
 
-  btnPopup3.addEventListener("click", () => {
-    objtracker = 2;
-  
-    let output = "";
-  
-    popupObj[objtracker].technologies.forEach((techContent) => {
-      output += `
+btnPopup3.addEventListener('click', () => {
+  objtracker = 2;
+
+  let output = '';
+
+  popupObj[objtracker].technologies.forEach((techContent) => {
+    output += `
           <span> ${techContent} </span>
       `;
-    });
-  
-    newElement.innerHTML = `
+  });
+
+  newElement.innerHTML = `
           <i class="fa fa-times" id="times"></i>
           <h2 class="popup_title">${popupObj[objtracker].name}</h2>
           <div class="prjct-info">
@@ -192,22 +191,22 @@ anypop.forEach(n => n.addEventListener("click", () => {
               </div>
           </div>
      `;
-     overLayContainer.style.display = "block"
-    popupContainer.style.display = "block";
-  });
+  overLayContainer.style.display = 'block';
+  popupContainer.style.display = 'block';
+});
 
-  btnPopup4.addEventListener("click", () => {
-    objtracker = 3;
-  
-    let output = "";
-  
-    popupObj[objtracker].technologies.forEach((techContent) => {
-      output += `
+btnPopup4.addEventListener('click', () => {
+  objtracker = 3;
+
+  let output = '';
+
+  popupObj[objtracker].technologies.forEach((techContent) => {
+    output += `
           <span> ${techContent} </span>
       `;
-    });
-  
-    newElement.innerHTML = `
+  });
+
+  newElement.innerHTML = `
           <i class="fa fa-times" id="times"></i>
           <h2 class="popup_title">${popupObj[objtracker].name}</h2>
           <div class="prjct-info">
@@ -232,7 +231,6 @@ anypop.forEach(n => n.addEventListener("click", () => {
               </div>
           </div>
      `;
-     overLayContainer.style.display = "block"
-    popupContainer.style.display = "block";
-  });
-
+  overLayContainer.style.display = 'block';
+  popupContainer.style.display = 'block';
+});
