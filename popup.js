07fecef9e3c7d143a4f9cb140eaec96c1e026699
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-04-08 20:06:13 +0300

    Checking and accessing local storage

--- popup.js
+++ popup.js
@@ -255,6 +255,22 @@ form.addEventListener('submit', (e) => {
 
 const nameInput = document.querySelector(".name");
 const textInput = document.querySelector(".message");
+
+function storageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return e instanceof DOMException && (
+      e.code === 22 || e.code === 1014 || e.name === 'QuotaExceededError' || e.name === 'NS_ERROR_DOM_QUOTA_REACHED') && (storage && storage.length !== 0
+    );
+  }
+}
+
 function toWebServer(){
   const formInputs = {
     inputName: nameInput.value,
@@ -272,3 +288,11 @@ function fromWebServer(){
     emailInput.value = formInputs.inputEmail;
     textInput.value = formInputs.inputText;
 }
+
+if (storageAvailable('localStorage')) {
+  fromWebServer();
+  nameInput.oninput = toWebServer;
+  emailInput.oninput = toWebServer;
+  textInput.oninput = toWebServer;
+}
+
